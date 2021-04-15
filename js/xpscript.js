51be5eb6cf1eb7d564122da8d3ed4a469b0a0261
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-04-16 00:03:30 +0200

    Implementato sezione minimizzata app, funzione di ri-comparsa app
    Implementato:
    - Icona e sezione minimizzata di minesweeper
    - Funzione ed animazione di ricomparsa minesweeper al click su sezione minimizzata

--- js/xpscript.js
+++ js/xpscript.js
@@ -1,22 +1,31 @@
+// Funzione apertura finestre
 function openWindow() {
     selectedIcon = $(this);
     
-    console.log();
     if (selectedIcon.find('#bin').length > 0) {
         console.log(selectedIcon.find('#bin'));
-
+    }else if(selectedIcon.find('#ie').length > 0){
+        console.log(selectedIcon.find('#ie'));
+    }else{
+        let window = $('#game');
+        if (window.hasClass('close')) {
+            console.log(window.hasClass('close'));
+            console.log(selectedIcon);
+            window.removeClass('hide');
+            window.removeClass('close');
+        }
     }
 }
 
-function closeWindow(elem) {
+// Funzione minimize finestra minesweepr
+function minimizeWindow(elem) {
     let window;
     window = elem.parents('#game');
     
     if (elem.parents('#game').length > 0) {
         setTimeout(function () {
             window.addClass('hide');
-            console.log(window);
-        }, 100);
+        }, 120);
         window.addClass('close');
     }
 }
@@ -24,9 +33,16 @@ function closeWindow(elem) {
 
 function init() {
     $('.icon-container').click(openWindow);
-
+    $('.minimized').click(openWindow);
+    
+    // $(window).resize(function () {
+    //     if (document.documentElement.clientWidth < 720) {
+    //         minimizeWindow($('.minimizeBtn'));
+    //     } 
+    // })
     $('#game .minimizeBtn').click(function () {
-        closeWindow($(this));
+        minimizeWindow($(this));
+        console.log($(this));
     })
 }
 
